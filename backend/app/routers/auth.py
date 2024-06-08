505ae49cdfc6021cde5b263c6ee12c406c8347bf
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-08 12:10:50 +0300

    Update datetime usage in create_access_token function

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -19,9 +19,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
