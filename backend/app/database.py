931d1b1d84639cbb7629b7c6fadc7cc8dd663728
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-08 18:31:03 +0300

    Update database.py imports and remove unused code

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
-
-# from databases import Database
 import os
 
 DATABASE_URL = os.getenv(
@@ -10,16 +7,5 @@ DATABASE_URL = os.getenv(
     "postgresql://postgres:password@host.docker.internal:5432/mydatabase",
 )
 
-# database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-@as_declarative()
-class Base:
-    id: int
-    __name__: str
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
