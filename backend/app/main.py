35eeeb9f20fa16a6e3244e342513d1ce90298f34
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-09 15:41:44 +0300

    Add CORS middleware to allow requests from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, points
 
 
 app = FastAPI()
+
+origins = [
+    "http://host.docker.internal:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(points.router)
