fe23554a27fd11fcf003c4a60d788e3ba25186ce
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-10 22:29:33 +0300

    Update authentication to add docstring

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -8,13 +8,20 @@ from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 
-
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
 def create_access_token(data: dict, expires_delta: timedelta = None):
+    """
+    Create an access token with the given data and expiration time.
+
+    Args:
+        data (dict): The data to be encoded in the token.
+        expires_delta (timedelta, optional): The expiration time for the token. Defaults to None.
+
+    Returns:
+        str: The encoded access token.
+    """
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now() + expires_delta
@@ -24,14 +31,25 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-
 router = APIRouter()
-
 
 @router.post("/token", response_model=schemas.Token)
 def login_for_access_token(
     db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
 ):
+    """
+    Generate an access token for the user based on their username and password.
+
+    Args:
+        db (Session): The database session.
+        form_data (OAuth2PasswordRequestForm): The form data containing the username and password.
+
+    Raises:
+        HTTPException: If the username or password is incorrect.
+
+    Returns:
+        Token: The access token.
+    """
     user = crud.get_user_by_username(db, form_data.username)
     if not user or not pwd_context.verify(form_data.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
@@ -41,9 +59,21 @@ def login_for_access_token(
     )
     return schemas.Token(access_token=access_token, token_type="bearer")
 
-
 @router.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    Create a new user.
+
+    Args:
+        user (UserCreate): The user data.
+        db (Session): The database session.
+
+    Raises:
+        HTTPException: If the username is already registered.
+
+    Returns:
+        User: The created user.
+    """
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
