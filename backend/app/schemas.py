f5b4fed3dca5d1649de7a08f92f134abeb5cc1d3
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-10 22:38:35 +0300

    Update models and schemas to include docstrings and improve type annotations

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -3,17 +3,28 @@ import datetime
 
 
 class PointBase(BaseModel):
+    """
+    Base model for a point.
+    """
     description: str
     latitude: str
     longitude: str
 
 
 class PointCreate(PointBase):
+    """
+    Model for creating a point.
+    Inherits from PointBase.
+    """
     id: int
     created_at: datetime.datetime
 
 
 class Point(PointBase):
+    """
+    Model for a point.
+    Inherits from PointBase.
+    """
     id: int
     created_at: datetime.datetime
     user_id: int
@@ -22,19 +33,33 @@ class Point(PointBase):
 
 
 class UserBase(BaseModel):
+    """
+    Base model for a user.
+    """
     username: str
 
 
 class UserCreate(UserBase):
+    """
+    Model for creating a user.
+    Inherits from UserBase.
+    """
     password: str
 
 
 class User(UserBase):
+    """
+    Model for a user.
+    Inherits from UserBase.
+    """
     id: int
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class Token(BaseModel):
+    """
+    Model for a token.
+    """
     access_token: str
     token_type: str
