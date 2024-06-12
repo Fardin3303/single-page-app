d628e324efcc98686b3d6a07e8a6071365b09bb9
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-12 10:47:13 +0300

    Update FastAPI app setup and CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing import List
 
 from app.routers import auth, points
+from app.custom_logging import LOGGER
 
 
-app = FastAPI()
+# Create FastAPI app
+app: FastAPI = FastAPI()
 
-origins = [
+# Set up CORS origins
+origins: List[str] = [
     "http://host.docker.internal:3000",
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -17,6 +20,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth.router)
 app.include_router(points.router)
+
+if __name__ == "__main__":
+    LOGGER.info("Starting the application.")
