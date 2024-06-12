c6cfd30a55b70e68140af5c0e299742f62a43f46
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-12 11:41:40 +0300

    Update custom logging configuration and initialization

--- backend/app/custom_logging.py
+++ backend/app/custom_logging.py
@@ -1,6 +1,37 @@
 import logging
+import sys
+import os
 
-# Initialize logger
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-LOGGER: logging.Logger = logging.getLogger(__name__)
+
+log_level = None
+
+
+def get_logger(name):
+    global log_level
+    if log_level is None:
+        init_logging()
+
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    return logger
+
+
+def init_logging():
+    global log_level
+
+    log_level_name = os.getenv('LOG_LEVEL', 'info')
+    log_level_name = log_level_name.lower()
+    if log_level_name == 'info':
+        log_level = logging.INFO
+    elif log_level_name == 'debug':
+        log_level = logging.DEBUG
+    elif log_level_name == 'warning':
+        log_level = logging.WARNING
+    elif log_level_name == 'error':
+        log_level = logging.ERROR
+    else:
+        logging.critical(f'Unhanled log level {log_level_name}')
+        sys.exit()
+
+    log_format = "%(asctime)s :: %(filename)s:%(lineno)d :: %(levelname)s :: %(message)s"
+    logging.basicConfig(format=log_format)
