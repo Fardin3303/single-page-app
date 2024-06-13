381cb95cc3e5524e901539b4ce25d77abc34e05a
Fardin Esmaeili <fardin.esmaeilikargani@afry.com>
2024-06-13 09:53:30 +0300

    Clean up

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ LOGGER.info("Setting up the fastapi app")
 # Create FastAPI app
 app: FastAPI = FastAPI()
 
-# Set up CORS origins
+# Set up CORS origins to allow requests from the frontend
 origins: List[str] = [
     "http://host.docker.internal:3000",
 ]
